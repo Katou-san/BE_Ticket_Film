294b979f915850283043d67b8c93befaa946e470
katou <96455212+Katou-san@users.noreply.github.com>
2024-03-23 22:32:18 +0700

    v2.6.1

--- src/index.js
+++ src/index.js
@@ -6,12 +6,21 @@ const { Connection } = require("./Config");
 
 const routes = require("./Routers/index");
 const { Query } = require("./Utils/Fun_SQL");
-app.use(cors({}));
+const { post } = require("./Routers/Router_Test");
+app.use(
+  cors({
+    origin: ["https://be-ticket-film-cfzfld22z-katous-projects.vercel.app"],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(bodyParser.json());
 
 routes(app);
-
+app.use("/", () => {
+  console.log("hello world");
+});
 app.listen(8080, async () => {
   try {
     console.log("Server is running in http://localhost:8080");
